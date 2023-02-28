56a88f36f666d5c3a2ffdc58a3824036191341a2
uxanovav <uxanovav@yandex.ru>
2023-03-01 01:26:31 +0300

    [#8] Initialize frontend, added shared components workspace

---
+++ apps/admin/components/pages/SignIn/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const SignIn: FC = () => {
+  return <>SignIn page</>;
+};
+
+export default SignIn;
--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,17 +1,32 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import createCache from '@emotion/cache';
 import './styles.css';
+import { CacheProvider, EmotionCache, ThemeProvider } from '@emotion/react';
+import theme from '@pizza-shop/shared-frontend-components/src/lib/mui-theme';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+type IAppPropertiesWithEmotion = AppProps & {
+  emotionCache?: EmotionCache;
+};
+
+const clientSideEmotionCache = createCache({ key: 'css' });
+
+function CustomApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: IAppPropertiesWithEmotion) {
   return (
-    <>
-      <Head>
-        <title>Welcome to admin!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Welcome to admin!</title>
+        </Head>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
